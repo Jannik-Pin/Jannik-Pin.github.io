7517ad3f6f47937324f6259b145d4788a3f8cd87
Jannik-Pin <54618685+Jannik-Pin@users.noreply.github.com>
2019-08-30 06:51:15 +0800

    Update designs.js

--- designs.js
+++ designs.js
@@ -19,6 +19,17 @@ sizePicker.addEventListener("click", (event) => {
 
 });
 
+
+/**
+*make Grid is creating a new grid 
+*height and width stand for cells and rows of the new grid 
+*we start with r=0. as for r being smaller than the figure of the height, increase r by one 
+*let the row increase by the number of r (input into the height field ) 
+*for cells the same as for rows (start at0, continue as long as width greater r and increase r by 1 
+* cell increase by one for every cell (width input) 
+*add an event listener for every cell when clicked the background color of this cell will be in the chosen color. 
+*only that cell and only for that color
+*/
 function makeGrid(height, width) {
 
 for (let r = 0; r < height; r++) {
