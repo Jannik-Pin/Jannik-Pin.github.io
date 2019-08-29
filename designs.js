36e155e573f8e69d6cdd5ff62b2e4ebeb31524ad
Jannik-Pin <54618685+Jannik-Pin@users.noreply.github.com>
2019-08-29 19:02:31 +0800

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,5 +1,5 @@
 var sizePicker = document.querySelector("#sizePicker");
-var canvas = document.querySelector("#pixelCanvas");
+var table = document.querySelector("#pixelCanvas");
 var color = document.querySelector("#colorPicker");
 var height = document.querySelector("#inputHeight").value;
 var width = document.querySelector("#inputWidth").value;
@@ -13,7 +13,7 @@ sizePicker.addEventListener("click", (event) => {
   let height = document.getElementById("inputHeight").value;
   let width = document.getElementById("inputWidth").value;
  
-  canvas.firstElementChild.remove();
+  table.firstElementChild.remove();
 
   makeGrid(height, width);
 
